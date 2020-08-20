01b1579a5275f91b409891d59d524feb9a4f53dc
Bilge Adam <bilge.adam@baogrenci.com>
2020-08-20 11:27:51 +0300

    Add project files.

---
+++ ConvertDataType/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string data = "Hello World!";
+            //data = "Bilge Adam";
+
+            //Ekrana "Bilge Adam" yazar
+            //Console.WriteLine(data);
+            //Console.ReadLine();
+
+
+
+            //Console.WriteLine("İsminizi giriniz");
+            //string ad = Console.ReadLine();
+
+            //Console.WriteLine("İsminiz: " + ad);
+            //Console.ReadLine();
+
+            //Console dan girilen sayının karesini ekrana yazdıran program
+
+            //Console.WriteLine("Lütfen bir tam sayı giriniz. Karesini alacağız da...");
+
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            ////int sayi = Convert.ToInt32(data);
+
+            //int sonuc = sayi * sayi;
+
+            //Console.WriteLine("Sayının karesi :" + sonuc);
+            //Console.ReadLine();
+
+            //"2" => 2
+
+
+            //Dışarıdan girdiğin 2 sayının çarpımını ekrana yazdır
+
+            //Console.WriteLine("Çarpılacak 2 sayıyı giriniz...");
+            //int sayi1 = Convert.ToInt32(Console.ReadLine());
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //int carpimsonuc = sayi1 * sayi2;
+
+            //Console.WriteLine(carpimsonuc);
+            //Console.ReadLine();
+
+
+            //Dışarıdan girdiğin 2 sayının bölümünü ekrana yazdır. ( Ondalıklı olabilir )
+            //Console.WriteLine("Bölüm için 2 sayıyı giriniz");
+            //double sayi1 = Convert.ToDouble(Console.ReadLine());
+            //double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sonuc = sayi1 / sayi2;
+
+            //Console.WriteLine("Bölüm sonucu: " + sonuc);
+            //Console.ReadLine();
+
+
+            //int sayi = 5;
+            //int x;
+            //sayi = sayi + 1;
+
+            //sayıyı önce atar sonra arttırır.
+            //x = sayi++;
+
+            //sayıyı 1 azaltır
+            //sayi--;
+
+            //sayıyı 1 arttırır
+            //sayi++;
+
+            //Console.WriteLine("Sayınız: " + sayi);
+            //Console.ReadLine();
+
+
+            //bool online = true;
+
+            //bool durum = 5 > 3;
+            //bool durum = 5 >= 3;
+
+            //Console.WriteLine(durum);
+            //Console.ReadLine();
+
+
+            //int sayi1 = 10;
+            //int sonuc = sayi1 % 3;
+
+            //Console.WriteLine(sonuc);
+            //Console.ReadLine();
+
+
+            int sayi = 5;
+            int x = sayi++;
+
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+    }
+}
